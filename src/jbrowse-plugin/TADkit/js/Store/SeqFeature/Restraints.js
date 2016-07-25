28e0d41c4f0a2dadf551fcc955d2eeff608835e9
David <david.castillo@cnag.crg.eu>
2016-07-25 20:22:06 +0200

    jbrowse plugin with restraints

--- src/jbrowse-plugin/TADkit/js/Store/SeqFeature/Restraints.js
+++ src/jbrowse-plugin/TADkit/js/Store/SeqFeature/Restraints.js
@@ -15,47 +15,81 @@ function(
     return declare(SeqFeatureStore, {
         getFeatures: function(query, featureCallback, finishCallback, errorCallback) {
             var $scope = parent.angular.element( parent.document.querySelector( '#jbrowse-iframe' ) ).scope();
+            if($scope.settings.current.chromStart > query.end || $scope.settings.current.chromEnd < query.start) {
+            	return;
+            }
             var harmonicsColor = $scope.overlay.palette[0];
             var lowerBoundsColor = $scope.overlay.palette[1];
             var resolution = $scope.settings.current.segmentLength*$scope.settings.current.particleSegments;
-            //var f;
-            // for(var i=0;i<$scope.settings.current.particlesCount;i++) {
-            //     var start = $scope.settings.current.chromStart+resolution*(i);
-            //         var end = $scope.settings.current.chromStart+resolution*(i+1);
-
-            //         var restraint = new SimpleFeature({
-            //             id: i+1,
-            //             data: {
-            //                 start: start,
-            //                 end: end,
-            //                 color: 'rgba(0,0,0,0)',
-            //                 score: 0,
-            //                 opacity: 0,
-            //                 name: ''
-            //             }
-            //         });
-            //         featureCallback(restraint);
-            // }
+            /*var query_start = Math.round((query.start-$scope.settings.current.chromStart)/resolution);
+            if(query_start<0) query_start = 0;
+            var query_end = Math.round((query.end-$scope.settings.current.chromStart)/resolution);
+            if(query_end>=$scope.settings.particlesCount) query_end = 0;
+            var f;
+            for(var i=query_start;i<query_end;i++) {
+                 var start = $scope.settings.current.chromStart+resolution*(i);
+                 var end = $scope.settings.current.chromStart+resolution*(i+1);
+                 
+                 var restraint = new SimpleFeature({
+                         id: i+1,
+                         data: {
+                             start: start,
+                             end: end,
+                             particle: 0,
+                             color: '',
+                             score: 0,
+                             opacity: 0,
+                             name: ''
+                         }
+                     });
+                     featureCallback(restraint);
+            }*/
             array.forEach($scope.data.harmonics, function(f) {
-                    var start = $scope.settings.current.chromStart+resolution*(f[1]-1);
-                    var end = $scope.settings.current.chromStart+resolution*(f[1]);
-
-                    var restraint = new SimpleFeature({
-                        id: f[1],
+            	var start = $scope.settings.current.chromStart+resolution*(f[0]);
+                var end = $scope.settings.current.chromStart+resolution*(f[0]+0.9);
+                var active = false;
+                if($scope.settings.current.particle == f[1]) active = true;
+                if(start>=query.start && end<=query.end) {
+                    var restraint1 = new SimpleFeature({
+                        id: 'h-'+f[0]+'-'+f[1],
                         data: {
                             start: start,
                             end: end,
-                            particle: f[0],
+                            particle_from: f[0],
+                            particle_to: f[1],
                             color: harmonicsColor,
                             score: f[3],
                             opacity: Math.round((f[3]/2)*(f[3]/2)*100)/100,
-                            name: 'Harmonic '+f[0]+'<-->'+f[1]
+                            name: 'Harmonic '+f[0]+'<-->'+f[1],
+                            active: active
                         }
                     });
-                    featureCallback(restraint);
+                    featureCallback(restraint1);
+                }
+                start = $scope.settings.current.chromStart+resolution*(f[1]);
+                end = $scope.settings.current.chromStart+resolution*(f[1]+0.9);
+                active = false;
+                if($scope.settings.current.particle == f[0]) active = true;
+                if(start>=query.start && end<=query.end) {
+                    var restraint2 = new SimpleFeature({
+                        id: 'h-'+f[1]+'-'+f[0],
+                        data: {
+                            start: start,
+                            end: end,
+                            particle_from: f[1],
+                            particle_to: f[0],
+                            color: harmonicsColor,
+                            score: f[3],
+                            opacity: Math.round((f[3]/2)*(f[3]/2)*100)/100,
+                            name: 'Harmonic '+f[1]+'<-->'+f[0],
+                            active: active
+                        }
+                    });
+                    featureCallback(restraint2);
+                }
             });
-            
             finishCallback();
+
         }
     });
 });
--- src/jbrowse-plugin/TADkit/js/View/Track/TADkitRestraints.js
+++ src/jbrowse-plugin/TADkit/js/View/Track/TADkitRestraints.js
@@ -2,7 +2,10 @@ define([
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
+    'dojo/_base/event',
+    'JBrowse/Util',
     'dojo/query',
+    'dojo/on',
     'dijit/registry',
     'JBrowse/View/Track/CanvasFeatures',    
 ],
@@ -10,7 +13,10 @@ function(
     declare,
     array,
     lang,
+    domEvent,
+    Util,
     query,
+    on,
     registry,
     CanvasFeatureTrack
 ) {
@@ -21,14 +27,21 @@ function(
             var $scope = parent.angular.element( parent.document.querySelector( '#jbrowse-iframe' ) ).scope();
             $scope.$watch('settings.current.particle', function(newPosition, oldPosition) {
                     if ( newPosition !== oldPosition ) {
-                        track._refreshTrack(track);
+                        track._refreshTrack(track, newPosition);
                     }
             });
         },
-        _refreshTrack: function(track) {
-            //track.glyphsLoaded = [];
+        _defaultConfig: function() {
+            return Util.deepUpdate(
+				lang.clone(this.inherited(arguments)),
+				                {
+				                    glyph: 'TADkit/View/FeatureGlyph/ArrowRestraint'				                    
+				                });
+        },
+        _refreshTrack: function(track, newPosition) {
+        	var context;
             array.forEach(this.blocks, function(block,i) {
-                            if( ! block )
+            				if( !block || !block.fRectIndex || !block.featureCanvas)
                                 return;
                             args = {
                                 block: block, 
@@ -39,14 +52,145 @@ function(
                             }
                             var ctx = block.featureCanvas.getContext('2d');
                             ctx.clearRect(0, 0, block.featureCanvas.width, block.featureCanvas.height);
-                            delete block.fRectIndex
-                            track.fillBlock(args);
-                            //block.destroy();
+                            var context = track.getRenderingContext( args );
+                            var idx = block.fRectIndex.byID;
+                            for( var id in idx ) {
+                                 var fRect = idx[id];
+                                 if(fRect.f.data.particle_from == newPosition || fRect.f.data.particle_to == newPosition) {
+                                	 fRect.f.data.active = true;
+                                	 fRect.glyph.renderFeature(context, fRect);  	 
+                                 } else {
+                                	 fRect.f.data.active = false;
+                                 }
+                            }
+                            
                         });
-            //track.updateStaticElements({ x: 0 });
+            
         },
-        _finish_callback: function() {
-            console.log('whatever');
+        _attachMouseOverEvents: function( ) {
+            var gv = this.browser.view;
+            var thisB = this;
+
+            if( this.displayMode == 'collapsed' ) {
+                if( this._mouseoverEvent ) {
+                    this._mouseoverEvent.remove();
+                    delete this._mouseoverEvent;
+                }
+
+                if( this._mouseoutEvent ) {
+                    this._mouseoutEvent.remove();
+                    delete this._mouseoutEvent;
+                }
+            } else {
+                if( !this._mouseoverEvent ) {
+                    this._mouseoverEvent = this.own( on( this.staticCanvas, 'mousemove', function( evt ) {
+                        evt = domEvent.fix( evt );
+                        var bpX = gv.absXtoBp( evt.clientX );
+                        var feature = thisB.layout.getByCoord( bpX, ( evt.offsetY === undefined ? evt.layerY : evt.offsetY ) );
+                        thisB.mouseoverFeature( feature, evt );
+                    }))[0];
+                }
+
+                if( !this._mouseoutEvent ) {
+                    this._mouseoutEvent = this.own( on( this.staticCanvas, 'mouseout', function( evt) {
+                        thisB.mouseoverFeature( undefined );
+                    }))[0];
+                }
+            }
+        },
+        mouseoverFeature: function( feature, evt ) {
+
+            if( this.lastMouseover == feature )
+                return;
+
+            if( evt )
+                var bpX = this.browser.view.absXtoBp( evt.clientX );
+
+            if( this.labelTooltip)
+                this.labelTooltip.style.display = 'none';
+
+            array.forEach( this.blocks, function( block, i ) {
+                if( ! block )
+                    return;
+                var context = this.getRenderingContext({ block: block, leftBase: block.startBase, scale: block.scale });
+                if( ! context )
+                    return;
+
+                if( this.lastMouseover && block.fRectIndex ) {
+                    var r = block.fRectIndex.getByID( this.lastMouseover.id() );
+                    //if( r )
+                    //    this.renderFeature( context, r );
+                }
+
+                if( block.tooltipTimeout )
+                    window.clearTimeout( block.tooltipTimeout );
+
+                if( feature ) {
+                    var fRect = block.fRectIndex && block.fRectIndex.getByID( feature.id() );
+                    if( ! fRect )
+                        return;
+
+                    if( block.containsBp( bpX ) ) {
+                        var renderTooltip = dojo.hitch( this, function() {
+                            if( !this.labelTooltip )
+                                return;
+                            var label = fRect.label || fRect.glyph.makeFeatureLabel( feature );
+                            var description = fRect.description || fRect.glyph.makeFeatureDescriptionLabel( feature );
+
+                            if( ( !label && !description ) )
+                                return;
+
+                            if( !this.ignoreTooltipTimeout ) {
+                                this.labelTooltip.style.left = evt.clientX + "px";
+                                this.labelTooltip.style.top = (evt.clientY + 15) + "px";
+                            }
+                            this.ignoreTooltipTimeout = true;
+                            this.labelTooltip.style.display = 'block';
+                            var labelSpan = this.labelTooltip.childNodes[0],
+                                descriptionSpan = this.labelTooltip.childNodes[1];
+
+                            if( this.config.onClick&&this.config.onClick.label ) {
+                                var context = lang.mixin( { track: this, feature: feature, callbackArgs: [ this, feature ] } );
+                                labelSpan.style.display = 'block';
+                                labelSpan.style.font = label.font;
+                                labelSpan.style.color = label.fill;
+                                labelSpan.innerHTML = this.template( feature, this._evalConf( context, this.config.onClick.label, "label" ) );
+                                return;
+                            }
+                            if( label ) {
+                                labelSpan.style.display = 'block';
+                                labelSpan.style.font = label.font;
+                                labelSpan.style.color = label.fill;
+                                labelSpan.innerHTML = label.text;
+                            } else {
+                                labelSpan.style.display = 'none';
+                                labelSpan.innerHTML = '(no label)';
+                            }
+                            if( description ) {
+                                descriptionSpan.style.display = 'block';
+                                descriptionSpan.style.font = description.font;
+                                descriptionSpan.style.color = description.fill;
+                                descriptionSpan.innerHTML = description.text;
+                            }
+                            else {
+                                descriptionSpan.style.display = 'none';
+                                descriptionSpan.innerHTML = '(no description)';
+                            }
+                        });
+                        if( this.ignoreTooltipTimeout )
+                            renderTooltip();
+                        else
+                            block.tooltipTimeout = window.setTimeout( renderTooltip, 600);
+                    }
+
+                    //fRect.glyph.mouseoverFeature( context, fRect );
+                    this._refreshContextMenu( fRect );
+                } else {
+                    block.tooltipTimeout = window.setTimeout( dojo.hitch(this, function() { this.ignoreTooltipTimeout = false; }), 200);
+                }
+            }, this );
+
+            this.lastMouseover = feature;
         },
         _createTadkitNavigation: function() {
             if(parent.angular==undefined) return false;
