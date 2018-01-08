7a466b5b5f9f7c0e16bf352c6acbcfd5357cb5c6
David <david.castillo@cnag.crg.eu>
2018-01-08 17:42:55 +0100

    Start menu on top

--- src/layout/topbar.controller.js
+++ src/layout/topbar.controller.js
@@ -4,7 +4,7 @@
 		.module('TADkit')
 		.controller('TopbarController', TopbarController);
 
-	function TopbarController($state, $scope, $mdSidenav) {
+	function TopbarController($state, $scope, $mdSidenav, $mdDialog) {
 
 		$scope.$state = $state;
 		if ($state.includes('main.project')){
@@ -18,9 +18,17 @@
 		$scope.toggleRight = function() {
 			$mdSidenav('right').toggle();
 		};
-		$scope.showDatasetCluster = $state.includes('browser');
-		$scope.$watch('current.model', function(newModel, oldModel) { 
-			if(typeof(newModel) == 'undefined') $scope.showDatasetCluster = false;
-		});
+		$scope.sampleAction = function(name, ev) {
+	      $mdDialog.show($mdDialog.alert()
+	        .title(name)
+	        .textContent('You triggered the "' + name + '" action')
+	        .ok('Great')
+	        .targetEvent(ev)
+	      );
+	    };
+		//$scope.showDatasetCluster = $state.includes('browser');
+		//$scope.$watch('current.model', function(newModel, oldModel) { 
+		//	if(typeof(newModel) == 'undefined') $scope.showDatasetCluster = false;
+		//});
 	}
 })();
